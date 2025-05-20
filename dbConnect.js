9bb214653675b2e5be703cb1dca16915c6106bf0
me32face <me32face@gmail.com>
2025-05-20 16:40:59 +0530

    Configured MongoDB Atlas and updated env variables

--- dbConnect.js
+++ dbConnect.js
@@ -1,8 +1,19 @@
-const mongoose=require("mongoose")
-mongoose.connect("mongodb://127.0.0.1/TechBlog")
-var db=mongoose.connection
-db.on("error",console.error.bind("error"))
-db.once("open",function(){
-    console.log("connection successfull");
-})
-module.export=db;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.MONGODB_URI;  // from .env
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+  console.log('MongoDB connection successful');
+});
+
+module.exports = db;
